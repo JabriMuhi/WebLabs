5cc3889f6b075bd7d33f14b5e73f85cfdb9da6cf
JabriMuhi <73785841+JabriMuhi@users.noreply.github.com>
2022-10-31 08:01:52 +0300

    feat: added comment to script

--- scripts/page-load.js
+++ scripts/page-load.js
@@ -1,3 +1,7 @@
+/**
+ * @description Скрипт отвечает за подсчет скорости загрузки сайта, информация выводится на каждой странице сайта.
+ */
+
 window.onload = function () {
     var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
     document.getElementById("page-load").innerHTML = ('Page load time is '+ loadTime + 'ms');
